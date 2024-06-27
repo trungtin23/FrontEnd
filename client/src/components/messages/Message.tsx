6ea7dd5444f32fc9e00d91eece2b0fbfdf28c263
Mai Xuân Thức <xuanthuc254@gmail.com>
2024-06-27 23:23:03 +0700

    modify UI

--- client/src/components/messages/Message.tsx
+++ client/src/components/messages/Message.tsx
@@ -11,30 +11,44 @@ interface ChatMessage {
   timestamp: string;
 }
 
-const Message: React.FC<MessageProps> = ({ webSocket, chatName }) => {
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
+
+const Message: React.FC = () => {
 
 
 
   return (
-      <div className='p-4'>
-        {messages.map((msg, index) => (
-            <div key={index} className={`chat ${msg.sender === 'Obi-Wan Kenobi' ? 'chat-start' : 'chat-end'}`}>
+      <div className='p-4 '>
+          <div className="chat chat-start">
               <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                  <img alt="avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                </div>
+                  <div className="w-10 rounded-full">
+                      <img
+                          alt="Tailwind CSS chat bubble component"
+                          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
               </div>
               <div className="chat-header">
-                {msg.sender}
-                <time className="text-xs opacity-50 ml-2">{msg.timestamp}</time>
+                  Obi-Wan Kenobi
+                  <time className="text-xs opacity-50">12:45</time>
               </div>
-              <div className="chat-bubble">{msg.message}</div>
-              <div className="chat-footer opacity-50">
-                {msg.sender === 'Obi-Wan Kenobi' ? 'Delivered' : 'Seen'}
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Delivered</div>
+          </div>
+          <div className="chat chat-end">
+              <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                      <img
+                          alt="Tailwind CSS chat bubble component"
+                          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
               </div>
-            </div>
-        ))}
+              <div className="chat-header">
+                  Anakin
+                  <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+          </div>
+
       </div>
   );
 };
--- client/src/components/messages/Messages.tsx
+++ client/src/components/messages/Messages.tsx
@@ -3,18 +3,14 @@ import Message from './Message';
 import MessageInput from './MessageInput';
 
 const Messages = () => {
-  const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
 
 
   return (
       <div className='overflow-auto'>
-        {webSocket && (
-            <>
-              <Message webSocket={webSocket} chatName="SomeChatName" />
-
-              <MessageInput webSocket={webSocket} />
-            </>
-        )}
+       <Message  />
+          <Message  />
+          <Message  />
+          <Message  />
       </div>
   );
 };
