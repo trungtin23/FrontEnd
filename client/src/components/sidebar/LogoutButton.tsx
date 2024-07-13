d9c02de42b2b137cde3b7da0b558438f6a1d3c15
Mai Xuân Thức <xuanthuc254@gmail.com>
2024-07-13 20:53:49 +0700

    logout

--- client/src/components/sidebar/LogoutButton.tsx
+++ client/src/components/sidebar/LogoutButton.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CiLogout } from 'react-icons/ci';
+import useLogout from "../../hooks/useLogout";
 
 
 
 const LogoutButton: React.FC = () => {
-    const navigate = useNavigate();
+    const {loading, logout} = useLogout();
 
 
 
     return (
         <div className='flex items-end mt-auto cursor-pointer' >
             <div>
-                <CiLogout />
-            </div>
-            <p className='ml-2 text-sm'>Đăng xuất</p>
+                        <CiLogout />
+                    </div>
+            <p onClick={logout} className='ml-2 text-sm'>Đăng xuất</p>
         </div>
     );
 };
